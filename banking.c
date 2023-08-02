16b3f70caf621fd03cb2cafd96621dce4cc070a9
Zealth121 <C.riesmeyer7@outlook.com>
2023-08-01 20:58:49 -0400

    created general structure for this program. I will not be working on this project for a few days due to surgery lol. May have one or two more commits until then.

--- banking.c
+++ banking.c
@@ -1,5 +1,64 @@
 #include <stdio.h>
 
+struct bank_account{
+
+    int accountPin;
+    int accountNumber;
+    int accountBalance;
+};
+
+struct bank_user{
+
+    char username[30];
+    char password[15];
+    struct bank_account accounts[3];
+};
+
+void bank_user_creation();
+void bank_user_login();
+void bank_account_creation();
+void bank_account_login();
+
 int main(){
-    
+
+    char response;
+
+    printf("\nWelcome to Zealth bank!\n\n");
+    do{
+
+        printf("What action would you like to take?\n");
+        printf("1. new bank user\n");
+        printf("2. user log in\n");
+        printf("3. close application\n");
+        scanf(" %c", &response);
+
+        switch(response){
+            case '1':
+                bank_user_creation();
+                break;
+            case '2':
+                bank_user_login();
+                break;
+            case '3':
+                break;
+            default:
+                printf("You have not chosen a valid response\n");
+                break;
+        };
+    }while(response != '3');
+
+    printf("\nThank you for using Zealth bank!\n");
+    return 0;
+}
+void bank_user_creation(){
+
+}
+void bank_user_login(){
+
+}
+void bank_account_creation(){
+
+}
+void bank_account_login(){
+
 }
